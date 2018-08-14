37e4a1cec5e1dc9cd93b6459a70c3c36195305ed
Ngo Van Tuan <tuannvd00538@fpt.edu.vn>
2018-08-14 13:46:09 +0700

    update

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const admin = require('firebase-admin');
 
 var serviceAccount = require("./iSilent.json");
 
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: '577731',
+  key: '9090d5fdcdf5d65162ff',
+  secret: '96ef6ce8e41763293cfb',
+  cluster: 'ap1',
+  encrypted: true
+});
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://i-silent.firebaseio.com"
@@ -21,6 +31,23 @@ app.use(express.static('./public'));
 var accountRoute = require('./routes/accountRoute');
 accountRoute(app);
 
+app.get('/test', (req, res) => {
+  return res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/pusher/auth", (req, res) => {
+  const socketId = req.body.socket_id;
+  const channel = req.body.channel_name;
+  var presenceData = {
+    user_id:
+      Math.random()
+        .toString(36)
+        .slice(2) + Date.now()
+  };
+  const auth = pusher.authenticate(socketId, channel, presenceData);
+  res.send(auth);
+});
+
 app.listen(8080, function () {
-	console.log('Hello World!');
+  console.log('Hello World!');
 });
